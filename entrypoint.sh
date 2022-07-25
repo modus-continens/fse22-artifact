6ecf4c3d96ef7e572c0e429d0b793b15acaa77cf
Chris <99123566+cyber-chris@users.noreply.github.com>
2022-07-25 19:39:33 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+LOCAL_IP_ADDR=$(ip addr | grep inet | awk 'NR==2{ print $2; }' | head -c -4)
+sed -i "s/https:\/\/download.java.net/http:\/\/$LOCAL_IP_ADDR/g" facts.Modusfile
+echo 1234 | nginx
+sh
